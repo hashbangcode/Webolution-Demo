e93c68dd5ae4d0f50f2583243420d9bc239c945e
Philip Norton <phil.norton@weareaccess.co.uk>
2019-08-06 08:39:30 +0100

    Ensured all routes exist in the routing file.

--- src/routes.php
+++ src/routes.php
@@ -12,6 +12,9 @@ $app->get('/', '\Hashbangcode\WebolutionDemo\Controller\HomeController:home');
 $app->any('/adminer', '\Hashbangcode\WebolutionDemo\Controller\AdminerController:adminer');
 $app->any('/clear_database', '\Hashbangcode\WebolutionDemo\Controller\AdminerController:clearDatabase');
 
+// Text
+$app->get('/text_evolution', '\Hashbangcode\WebolutionDemo\Controller\TextController:textEvolution');
+
 // Number.
 $app->get('/number_evolution', '\Hashbangcode\WebolutionDemo\Controller\NumberController:numberEvolution');
 //$app->map(['GET', 'POST'], '/number_evolution_form', '\Hashbangcode\WebolutionDemo\Controller\NumberController:numberEvolutionForm');
@@ -24,6 +27,10 @@ $app->get('/color_evolution_database[/{step}]', '\Hashbangcode\WebolutionDemo\Co
 $app->get('/image_evolution', '\Hashbangcode\WebolutionDemo\Controller\ImageController:imageEvolution');
 $app->get('/image_evolution_database[/{step}]', '\Hashbangcode\WebolutionDemo\Controller\ImageDatabaseController:imageEvolution');
 
+// Page.
+$app->get('/page_evolution', '\Hashbangcode\WebolutionDemo\Controller\PageController:pageEvolution');
+
 // Dashboard
 $app->map(['get', 'post'], '/color_dashboard_evolution[/{params:.*}]', '\Hashbangcode\WebolutionDemo\Controller\ColorDashboardController:dashboardEvolution')->setName('color_dashboard_evolution');
 $app->map(['get', 'post'], '/image_dashboard_evolution[/{params:.*}]', '\Hashbangcode\WebolutionDemo\Controller\ImageDashboardController:dashboardEvolution')->setName('image_dashboard_evolution');
+$app->map(['get', 'post'], '/page_dashboard_evolution[/{params:.*}]', '\Hashbangcode\WebolutionDemo\Controller\PageDashboardController:dashboardEvolution')->setName('page_dashboard_evolution');
